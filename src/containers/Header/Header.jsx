56212652233ae19743781d2f8cc9fcb771c4593d
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-09 16:09:51 -0500

    Welcome image added

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { SubHeader } from '@/components';
 import './Header.scss';
+import Image from 'next/image';
 
 const Header = () => {
     return (
@@ -10,6 +11,9 @@ const Header = () => {
                 <h1 className='app__header-h1'>Your Kitchen Away From Home</h1>
                 <p className='p__opensans' style={{ margin: '2rem 0' }}>At Brewhemia, we blend the warmth of Mexican cuisine with the art of coffee. Savor our award-winning breakfast burritos with a cup of our finest brew.</p>
                 <button type='button' className='custom__button'>Explore Menu</button>
+            </div>
+            <div className='app__wrapper_img'>
+                <Image src="/welcome.png" alt='Latte and croissant on a table' width={1024} height={1024} />
             </div>
         </header>
     )
