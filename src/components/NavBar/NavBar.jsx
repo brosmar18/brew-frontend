c414433e907a57c696e905196c7869a96f0d1961
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-09 15:28:11 -0500

    NavBar func component created and added to global layout

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Link from 'next/link';
+import './NavBar.scss';
+import {MdMenu, MdRestaurantMenu} from 'react-icons/md'
+
+
+
+const NavBar = () => {
+  return (
+    <div>
+      <h1>NavBar</h1>
+    </div>
+  )
+}
+
+export default NavBar;
