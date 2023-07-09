4d18186899ac26e6a6b77e4573be25f28ee70e76
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-09 16:47:40 -0500

    Intro func component created and added to home page

---
+++ src/containers/Intro/Intro.jsx
@@ -0,0 +1,15 @@
+'use client';
+
+import React, {useState, useRef} from 'react'
+import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs';
+import './Intro.scss';
+
+const Intro = () => {
+  return (
+    <div>
+      <h1>Intro</h1>
+    </div>
+  )
+}
+
+export default Intro;
