2a00753e2192ff7e28916620309d37b63b172d93
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-09 18:37:26 -0500

    images added

--- src/containers/Footer/Footer.jsx
+++ src/containers/Footer/Footer.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { FooterOverlay, NewsLetter } from '@/components';
 import './Footer.scss';
+import Image from 'next/image';
+import { FiFacebook, FiTwitter, FiInstagram } from 'react-icons/fi';
 
 const Footer = () => {
     return (
@@ -14,6 +16,16 @@ const Footer = () => {
                     <p className="p__opensans">1202 3rd St SE #101, Cedar Rapids, IA 52401</p>
                     <p className="p__opensans">(319) 364-0802</p>
                     <p className="p__opensans">andrea@brewhemia.com</p>
+                </div>
+                <div className="app__footer-links_logo">
+                    <Image src='/logo.png' alt="footer_logo" width={150} height={20}/>
+                    <p className="p__opensans">&quot;The best way to find yourself is to lose yourself in the service of others.&quot;</p>
+                    <Image src='/spoon.png' className="spoon__img" style={{ marginTop: 15 }} width={45} height={10} />
+                    <div className="app__footer-links_icons">
+                        <FiFacebook />
+                        <FiTwitter />
+                        <FiInstagram />
+                    </div>
                 </div>
             </div>
         </div>
