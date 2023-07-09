96268fe7ff202ee0d1302f8f8408103d780ceacf
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-09 17:43:50 -0500

    images added

---
+++ src/constants/images.jsx
@@ -0,0 +1,8 @@
+export const images = {
+    gallery01: '/gallery01.png',
+    gallery02: '/gallery02.png',
+    gallery02: '/gallery03.png',
+    gallery02: '/gallery04.png',
+};
+
+export default images;
--- src/containers/Gallery/Gallery.jsx
+++ src/containers/Gallery/Gallery.jsx
@@ -3,6 +3,8 @@ import React, { useRef } from 'react';
 import { BsInstagram, BsArrowLeftShort, BsArrowRightShort } from 'react-icons/bs';
 import { SubHeader } from '@/components';
 import './Gallery.scss';
+import {images} from '@/constants/images.jsx';
+import Image from 'next/image';
 
 
 const Gallery = () => {
@@ -24,6 +26,10 @@ const Gallery = () => {
       <h1 className="headtext__cormorant">Photo Gallery</h1>
       <p className="p__opensans" style={{ color: '#AAAAAA', marginTop: '2rem' }}>Step into our world through the lens of our Photo Gallery. From our artisan coffee and vibrant Mexican dishes to our cozy café ambiance and heartwarming community moments, each picture tells a piece of the Brewhemia story. Enjoy the visual journey!</p>
       <button type="button" className="custom__button">Explore Menu</button>
+      </div>
+      <div className='app__gallery-images'>
+      <Image src={images.gallery01} alt='gallery' width={301} height={447}/>
+
       </div>
     </section>
   )
