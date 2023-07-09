fd646b34d489184fc577d83a12c43a3ec228e0f4
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-09 17:26:34 -0500

    SubHeader imported

--- src/containers/Gallery/Gallery.jsx
+++ src/containers/Gallery/Gallery.jsx
@@ -1,7 +1,9 @@
 'use client';
 import React, { useRef } from 'react';
 import { BsInstagram, BsArrowLeftShort, BsArrowRightShort } from 'react-icons/bs';
+import { SubHeader } from '@/components';
 import './Gallery.scss';
+
 
 const Gallery = () => {
   return (
