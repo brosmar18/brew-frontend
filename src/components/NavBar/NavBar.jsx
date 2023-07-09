c564f58d5d7950476572068fc0ae0d749a36f4ac
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-09 15:35:32 -0500

    logo image added

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
+'use client';
+
+import React, {useState} from 'react'
 import Link from 'next/link';
 import './NavBar.scss';
-import {MdMenu, MdRestaurantMenu} from 'react-icons/md'
+import {MdMenu, MdRestaurantMenu} from 'react-icons/md';
+import {motion} from 'framer-motion';
+import Image from 'next/image';
 
 
 
 const NavBar = () => {
   return (
-    <div>
-      <h1>NavBar</h1>
-    </div>
+    <nav className='nav'>
+        <div className='nav__logo'>
+        <Image src="/logo.png" alt='logo' width={150} height={20} className='nav__logo-img' />
+        </div>
+    </nav>
   )
 }
 
