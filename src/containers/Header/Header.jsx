d7aa6734119ce6046b6e16d95041c81178d9269e
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-09 16:05:17 -0500

    desc added

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
             <div className='app__wrapper_info'>
                 <SubHeader title="Brewing Passion in Every Pour" />
                 <h1 className='app__header-h1'>Your Kitchen Away From Home</h1>
-                
+                <p className='p__opensans' style={{ margin: '2rem 0' }}>At Brewhemia, we blend the warmth of Mexican cuisine with the art of coffee. Savor our award-winning breakfast burritos with a cup of our finest brew.</p>
             </div>
         </header>
     )
