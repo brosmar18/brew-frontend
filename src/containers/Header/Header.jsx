59fe203df6ec92d7a71d416603197762c18774d3
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-09 16:05:30 -0500

    button added

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -9,6 +9,7 @@ const Header = () => {
                 <SubHeader title="Brewing Passion in Every Pour" />
                 <h1 className='app__header-h1'>Your Kitchen Away From Home</h1>
                 <p className='p__opensans' style={{ margin: '2rem 0' }}>At Brewhemia, we blend the warmth of Mexican cuisine with the art of coffee. Savor our award-winning breakfast burritos with a cup of our finest brew.</p>
+                <button type='button' className='custom__button'>Explore Menu</button>
             </div>
         </header>
     )
