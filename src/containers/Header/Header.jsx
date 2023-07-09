3a5a15f048c4af5455d5d0101d5ad3aff03c6418
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-09 16:02:12 -0500

    Header basic func component built and exported to home page

---
+++ src/containers/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { SubHeader } from '@/components';
+import './Header.scss';
+
+const Header = () => {
+    return (
+        <header className='app__header app__wrapper section__padding'>
+            <h1>Header</h1>
+        </header>
+    )
+}
+
+export default Header
