78ef4dc48f11f5a611ca0914a29e367ba83ec6fa
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-09 17:26:53 -0500

    scroll logic added

--- src/containers/Gallery/Gallery.jsx
+++ src/containers/Gallery/Gallery.jsx
@@ -6,6 +6,16 @@ import './Gallery.scss';
 
 
 const Gallery = () => {
+  const scrollRef = useRef(null);
+  const scroll = (direction) => {
+    const { current } = scrollRef;
+
+    if (direction === 'left') {
+      current.scrollLeft -= 300;
+    } else {
+      current.scrollLeft += 300;
+    }
+  };
   return (
     <div>
       Gallery
