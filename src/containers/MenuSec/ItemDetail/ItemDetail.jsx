4ad3525b269d1d041e1c7f3f7798150dc92aec81
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-10 13:07:22 -0500

    ItemDetail added to menu page

---
+++ src/containers/MenuSec/ItemDetail/ItemDetail.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './ItemDetail.scss';
+
+const ItemDetail = () => {
+    return (
+        <h1>ItemDetail</h1>
+    )
+}
+
+export default ItemDetail;
--- src/containers/index.js
+++ src/containers/index.js
@@ -3,3 +3,4 @@ export {default as Intro} from './Intro/Intro';
 export {default as Gallery} from './Gallery/Gallery';
 export {default as Footer} from './Footer/Footer';
 export {default as MenuSec} from './MenuSec/MenuSec';
+export {default as ItemDetail} from './MenuSec/ItemDetail/ItemDetail';
