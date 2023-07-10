62a6dc9f5f9240db0b491ea6edb6c370d84ef5c8
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-10 12:23:45 -0500

    buttons added to menu items

--- src/containers/MenuSec/MenuSec.jsx
+++ src/containers/MenuSec/MenuSec.jsx
@@ -1,45 +1,51 @@
 import React from 'react'
 import { SubHeader, MenuItem } from '@/components';
-import {data} from '@/constants';
+import { data } from '@/constants';
 import Image from 'next/image';
 import './MenuSec.scss';
 
 const Menu = () => {
   return (
     <div className="app__specialMenu flex__center section__padding app__bg" id="menu">
-    <div className="app__specialMenu-title">
-      <SubHeader title="Menu that fits your palatte" />
-      <h1 className="headtext__cormorant">Our Menu</h1>
-    </div>
+      <div className="app__specialMenu-title">
+        <SubHeader title="Menu that fits your palatte" />
+        <h1 className="headtext__cormorant">Our Menu</h1>
+      </div>
 
-    <div className="app__specialMenu-menu">
-      <div className="app__specialMenu-menu_breakfast  flex__center">
-        <p className="app__specialMenu-menu_heading">Breakfast</p>
-        <div className="app__specialMenu_menu_items">
-          {data.breakfast.map((breakfastItem, index) => (
-            <MenuItem key={breakfastItem.title + index} title={breakfastItem.title} price={breakfastItem.price} desc={breakfastItem.desc} />
-          ))}
+      <div className="app__specialMenu-menu">
+        <div className="app__specialMenu-menu_breakfast  flex__center">
+          <p className="app__specialMenu-menu_heading">Breakfast</p>
+          <div className="app__specialMenu_menu_items">
+            {data.breakfast.map((breakfastItem, index) => (
+              <div>
+                <MenuItem key={breakfastItem.title + index} title={breakfastItem.title} price={breakfastItem.price} desc={breakfastItem.desc} />
+                <button type="button" className="custom__button">View More</button>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div className="app__specialMenu-menu_img">
+          <Image src='/menu.png' alt="menu__img" width={410} height={721} />
+        </div>
+
+        <div className="app__specialMenu-menu_lunch  flex__center">
+          <p className="app__specialMenu-menu_heading">Lunch</p>
+          <div className="app__specialMenu_menu_items">
+            {data.lunch.map((cocktail, index) => (
+              <div>
+                <MenuItem key={cocktail.title + index} title={cocktail.title} price={cocktail.price} desc={cocktail.desc} />
+                <button type="button" className="custom__button">View More</button>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
 
-      <div className="app__specialMenu-menu_img">
-        <Image src='/menu.png' alt="menu__img" width={410} height={721} />
-      </div>
-
-      <div className="app__specialMenu-menu_lunch  flex__center">
-        <p className="app__specialMenu-menu_heading">Lunch</p>
-        <div className="app__specialMenu_menu_items">
-          {data.lunch.map((cocktail, index) => (
-            <MenuItem key={cocktail.title + index} title={cocktail.title} price={cocktail.price} desc={cocktail.desc} />
-          ))}
-        </div>
+      <div style={{ marginTop: 15 }}>
+        <button type="button" className="custom__button">View More</button>
       </div>
     </div>
-
-    <div style={{ marginTop: 15 }}>
-      <button type="button" className="custom__button">View More</button>
-    </div>
-  </div>
   )
 }
 
