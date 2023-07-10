cad3eda12347cfd3dfd4b17e22c43ea31defeaf4
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-09 19:48:36 -0500

    menu added

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export {default as NavBar} from './NavBar/NavBar';
 export {default as SubHeader} from './SubHeader/SubHeader';
 export {default as FooterOverlay} from './Footer/FooterOverlay';
 export {default as NewsLetter} from './Footer/NewsLetter';
+export {default as MenuItem} from './MenuItem/MenuItem';
