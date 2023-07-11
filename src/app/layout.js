e81e8e792714a9deef6470d1a3d586e220a81201
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-10 19:13:27 -0500

    react-modal error fixed

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,19 +1,22 @@
+'use client';
 import './globals.css'
 import { Inter } from 'next/font/google'
 import {NavBar} from '@/components';
 import {Footer} from '@/containers';
+import { useEffect } from 'react';
+import Modal from 'react-modal';
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata = {
-  title: 'Brewhemia',
-  description: 'Brewhemia, coffee shop in Cedar Rapids, Iowa',
-}
 
 export default function RootLayout({ children }) {
+  useEffect(() => {
+    Modal.setAppElement('#root');
+  }, []);
+  
   return (
     <html lang="en">
-      <body className="page">
+      <body className="page" id="root">
         <NavBar />
         {children}
         <Footer />
