300909e8eb0083f16da7f931c3b8affb8a5fe164
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-12 15:41:54 -0500

    intro rmvd from home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,13 @@
 import Image from 'next/image'
 import styles from './page.module.css'
-import {Header, Intro, Gallery} from '@/containers';
+import {Header, Gallery} from '@/containers';
 
 
 export default function Home() {
   return (
     <div>
       <Header />
-      <Intro />
+      {/* <Intro /> */}
       <Gallery />
     </div>
   )
