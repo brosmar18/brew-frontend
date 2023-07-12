0084c8b6a9455ee82796b4176fd42ed16e66caac
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-12 13:18:50 -0500

    icons removed

--- src/containers/ItemDetail/ItemDetail.jsx
+++ src/containers/ItemDetail/ItemDetail.jsx
@@ -3,7 +3,6 @@
 import React from 'react';
 import './ItemDetail.scss';
 import Image from 'next/image';
-import { IoMdArrowBack, IoMdArrowForward, IoMdClose } from 'react-icons/io';
 import data from '@/constants/data';
 
 const ItemDetail = () => {
@@ -11,10 +10,13 @@ const ItemDetail = () => {
     return (
         <section className='menu-items flex__center section__padding app__bg'>
             <h2 className='headtext__cormorant'>The Food You've Been Craving</h2>
-            <div className='menu-items__layout'>
+            <div className='menu-items__tabs'>
                 {['breakfast', 'lunch', 'drinks'].map((item, index) => (
                     <p className='p__cormorant' key={index}>{item}</p>
                 ))}
+            </div>
+            <div className='menu-items__cards'>
+                    
             </div>
         </section>
     )
