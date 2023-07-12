0dd9e4fd07b75be610800698d862d23069f6e8a4
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-12 13:13:28 -0500

    selection tabs added

--- src/containers/ItemDetail/ItemDetail.jsx
+++ src/containers/ItemDetail/ItemDetail.jsx
@@ -10,6 +10,11 @@ const ItemDetail = () => {
     return (
         <section className='menu-items flex__center section__padding app__bg'>
             <h2 className='headtext__cormorant'>The Food You've Been Craving</h2>
+            <div className='menu-items__layout'>
+                {['breakfast', 'lunch', 'drinks'].map((item, index) => (
+                    <p className='p__cormorant' key={index}>{item}</p>
+                ))}
+            </div>
         </section>
     )
 }
