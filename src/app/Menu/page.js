1cfc48b5c96fb8382b5095d86d17ad9503cc70ea
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-12 13:10:17 -0500

    title added

--- src/app/Menu/page.js
+++ src/app/Menu/page.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {MenuSec} from '@/containers';
+import { ItemDetail } from '@/containers';
 
 const Menu = () => {
   return (
     <div>
       <MenuSec />
+      <ItemDetail />
     </div>
   )
 }
--- src/containers/ItemDetail/ItemDetail.jsx
+++ src/containers/ItemDetail/ItemDetail.jsx
@@ -5,33 +5,12 @@ import './ItemDetail.scss';
 import Image from 'next/image';
 import { IoMdArrowBack, IoMdArrowForward, IoMdClose } from 'react-icons/io';
 
-const ItemDetail = ({ item, closeModal, goBack, goForward, showBackArrow, showForwardArrow }) => {
-    if (!item) {
-        return null;
-    }
+const ItemDetail = () => {
 
     return (
-        <div className="item__modal app__bg">
-            <h1>{item.title}</h1>
-            <p>{item.price}</p>
-            <p>{item.desc}</p>
-            <Image src={item.imgUrl} alt={item.title} width={400} height={400} />
-            <div className='nav__buttons'>
-                {showBackArrow && (
-                    <button className="back-button" onClick={goBack}>
-                        <IoMdArrowBack size={24} />
-                    </button>
-                )}
-                <button className='close-button' onClick={closeModal}>
-                    <IoMdClose size={24} />
-                </button>
-                {showForwardArrow && (
-                    <button className="forward-button" onClick={goForward}>
-                        <IoMdArrowForward size={24} />
-                    </button>
-                )}
-            </div>
-        </div>
+        <section className='menu-items flex__center section__padding app__bg'>
+            <h2 className='headtext__cormorant'>The Food You've Been Craving</h2>
+        </section>
     )
 }
 
