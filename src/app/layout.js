3d84e1ce9705e540943edb2b96f88e6c7595a127
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-07-12 13:03:45 -0500

    modal code removed

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,18 +1,11 @@
-'use client';
 import './globals.css'
 import { Inter } from 'next/font/google'
-import {NavBar} from '@/components';
-import {Footer} from '@/containers';
-import { useEffect } from 'react';
-import Modal from 'react-modal';
+import { NavBar } from '@/components';
+import { Footer } from '@/containers';
 
 const inter = Inter({ subsets: ['latin'] })
 
-
 export default function RootLayout({ children }) {
-  useEffect(() => {
-    Modal.setAppElement('#root');
-  }, []);
   
   return (
     <html lang="en">
